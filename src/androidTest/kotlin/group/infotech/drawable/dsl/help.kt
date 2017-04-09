c234047621e9d4161efd95448b2f4772bca52f2a
adelnizamutdinov <stiggpwnz@gmail.com>
2017-04-09 16:25:32 +0300

    Problem: test assertions are noisy; Solution: replace expekt with kotlintest

--- src/androidTest/kotlin/group/infotech/drawable/dsl/help.kt
+++ src/androidTest/kotlin/group/infotech/drawable/dsl/help.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import io.kotlintest.matchers.Matcher
+import io.kotlintest.matchers.Result
 
 fun snapshot(d: Drawable): Bitmap {
 
@@ -23,3 +25,10 @@ typealias DrawableRes = Int
 @Suppress("DEPRECATION")
 fun Context.drawable(d: DrawableRes): Drawable =
     resources.getDrawable(d)
+
+fun drawPixelsLike(actual: Drawable): Matcher<Drawable> =
+    object : Matcher<Drawable> {
+      override fun test(value: Drawable): Result =
+          Result(passed = snapshot(value).sameAs(snapshot(actual)),
+                 message = "$value has different pixels than $actual")
+    }
